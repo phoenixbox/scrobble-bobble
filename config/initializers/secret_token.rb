76e96860365d0d9a4eab84b4ccbe53ca0cf9deed
Shane Rogers <rogerssh@tcd.ie>
2013-05-12 17:24:18 -0600

    first commit REDO

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScrobbleBobble::Application.config.secret_token = '78aaf694e8bdfe73cedb071b2beaa0d6c3a1493d71ed60001cf4d4528c10f8f074b6861496f13d51653823af7dfff451956b20b288a2a0c0450f8eff27df1999'
